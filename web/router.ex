2d1f8ce79953ab80bfe74ab68aee91f99932c585
vladkosarev <vladk@om-tech.net>
2016-02-06 13:21:18 -0500

    Adding http endpoint for seats

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,10 @@ defmodule SeatSaver.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", SeatSaver do
-  #   pipe_through :api
-  # end
+  scope "/api", SeatSaver do
+    pipe_through :api
+
+    resources "/seats", SeatController, except: [:new, :edit]
+
+  end
 end
